fb64672144649fbfb5f1df7d003875a50195a7f5
rahulparajuli <sr2rahul19@gmail.com>
2023-07-12 11:42:00 +0545

    ownership

--- scripts/nginx.sh
+++ scripts/nginx.sh
@@ -8,9 +8,9 @@ sudo cp /home/ubuntu/scraper/nginx/nginx.conf /etc/nginx/sites-available/scraper
 sudo ln -s /etc/nginx/sites-available/scraper /etc/nginx/sites-enabled/
 #sudo ln -s /etc/nginx/sites-available/blog /etc/nginx/sites-enabled
 #sudo nginx -t
-
+echo "stared nginx"
 sudo gpasswd -a www-data ubuntu
 sudo chown -R www-data:www-data /home/ubuntu/scraper
-
+echo "changed ownership"
 sudo systemctl restart nginx
 
