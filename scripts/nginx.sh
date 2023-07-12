305ca04f346acf3e3ac43074568b16992f9bad8d
rahulparajuli <sr2rahul19@gmail.com>
2023-07-12 11:41:11 +0545

    chown hpassword

--- scripts/nginx.sh
+++ scripts/nginx.sh
@@ -8,6 +8,9 @@ sudo cp /home/ubuntu/scraper/nginx/nginx.conf /etc/nginx/sites-available/scraper
 sudo ln -s /etc/nginx/sites-available/scraper /etc/nginx/sites-enabled/
 #sudo ln -s /etc/nginx/sites-available/blog /etc/nginx/sites-enabled
 #sudo nginx -t
+
 sudo gpasswd -a www-data ubuntu
+sudo chown -R www-data:www-data /home/ubuntu/scraper
+
 sudo systemctl restart nginx
 
