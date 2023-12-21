5bdcf05ceb1de5d1d7b13e2e03f62478522aed9e
Max Dotnet <maxbasecode@gmail.com>
2023-12-21 23:05:43 +0330

    'last-changes'

--- scraper/settings.py
+++ scraper/settings.py
@@ -132,8 +132,8 @@ import os
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-# MEDIA_ROOT = BASE_DIR
-# MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR
+MEDIA_URL = '/media/'
 
 
 # CORS_ORIGIN_WHITELIST = [
@@ -172,6 +172,6 @@ AWS_STORAGE_BUCKET_NAME = 'ads-scrap'
 AWS_S3_REGION_NAME = 'us-east-1'
 AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 AWS_DEFAULT_ACL = 'public-read'
-
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+# use below settings in production 
+# DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+# STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
